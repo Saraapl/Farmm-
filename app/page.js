c844668047aaf8c068c3e47c1165f9a523b425a1
Sara Pineda <spinedal@eafit.edu.co>
2024-11-24 19:58:31 -0500

    Proyecto creado!

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+ return (
+  <div>
+    <h1 className={styles.title}> ¡Bienvenido a Farmm! {
+    }</h1>
+  </div>
+ );
+}
